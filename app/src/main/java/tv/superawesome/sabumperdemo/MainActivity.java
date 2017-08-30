416699893321998c2cb44ba2ed05d5b9173e338d
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-08-30 14:01:28 +0100

    update

---
+++ app/src/main/java/tv/superawesome/sabumperdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package tv.superawesome.sabumperdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import tv.superawesome.lib.sabumperpage.SABumperPage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SABumperPage.overrideName("New name");
+        SABumperPage.overrideLogo(getResources().getDrawable(R.drawable.kws_white_700));
+        SABumperPage.setListener(new SABumperPage.Interface() {
+            @Override
+            public void didEndBumper() {
+                Log.d("SuperAwesome", "Did close");
+            }
+        });
+        SABumperPage.play(this);
+    }
+}
